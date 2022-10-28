65eced3f1783c597e5f8b5a278aabfe72f1a5608
Kim Seokju <cjkim10000@gmail.com>
2022-10-28 18:38:21 +0900

    Add serialEvnet on Sender part

--- arduino/sender/sender.ino
+++ arduino/sender/sender.ino
@@ -11,6 +11,7 @@ int total_bytes;
 void setup() {
   pinMode(TRANSMIT_LED,OUTPUT);
   total_bytes = strlen(text);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -36,6 +37,26 @@ void transmit_byte(char data_byte)
     digitalWrite(TRANSMIT_LED,(data_byte >> i) & 0x01); 
     delay(SAMPLING_TIME);
   }
- digitalWrite(TRANSMIT_LED,HIGH); //Return to IDLE state
+ dig
+ italWrite(TRANSMIT_LED,HIGH); //Return to IDLE state
  delay(SAMPLING_TIME);
+}
+
+void serialEvent() {
+  char inLocation = Serial.read();
+  if (Serial.available()) {
+
+    if (inLocation == '1') {
+      text = "10101010\n";
+      Serial.println("Location changed to 1");
+    } else if (inLocation == '2') {
+      text = "20202020\n";
+      Serial.println("Location changed to 2");
+    } else if (inLocation == '3') {
+      text = "30303030\n";
+      Serial.println("Location changed to 3");
+    } else {
+      Serial.println("No allowed location.");
+    }
+  }
 }
